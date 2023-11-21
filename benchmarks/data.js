02853063e0e672ec05519a574e83b7c35718d098
github-action-benchmark <github@users.noreply.github.com>
2023-11-21 03:06:39 +0000

    add Benchmark (go) benchmark result for 048feff76ebb6439968c2d3bae52ba80de46e1ca

---
+++ benchmarks/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1700535999113,
+  "repoUrl": "https://github.com/sunhailin-Leo/triton-service-go",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "379978424@qq.com",
+            "name": "LeoSun",
+            "username": "sunhailin-Leo"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "048feff76ebb6439968c2d3bae52ba80de46e1ca",
+          "message": "Update benchmark.yml",
+          "timestamp": "2023-11-21T11:05:55+08:00",
+          "tree_id": "0aba32b074d13497361970113e2d3f4d282afeab",
+          "url": "https://github.com/sunhailin-Leo/triton-service-go/commit/048feff76ebb6439968c2d3bae52ba80de46e1ca"
+        },
+        "date": 1700535998414,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkFullTokenizerNotChinese",
+            "value": 20551,
+            "unit": "ns/op\t   11752 B/op\t     288 allocs/op",
+            "extra": "58838 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFullTokenizerChinese",
+            "value": 7165,
+            "unit": "ns/op\t    4272 B/op\t     106 allocs/op",
+            "extra": "163981 times\n4 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
