0add8fe7a1e01cfc90c0c65a6734e7a168c713fa
github-action-benchmark <github@users.noreply.github.com>
2023-11-21 03:12:46 +0000

    add Benchmark (go) benchmark result for f5b1c32314a29e5808360e4be740e5de98e2b410

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1700535999113,
+  "lastUpdate": 1700536366291,
   "repoUrl": "https://github.com/sunhailin-Leo/triton-service-go",
   "entries": {
     "Benchmark": [
@@ -36,6 +36,42 @@ window.BENCHMARK_DATA = {
             "value": 7165,
             "unit": "ns/op\t    4272 B/op\t     106 allocs/op",
             "extra": "163981 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "379978424@qq.com",
+            "name": "LeoSun",
+            "username": "sunhailin-Leo"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f5b1c32314a29e5808360e4be740e5de98e2b410",
+          "message": "Merge pull request #37 from sunhailin-Leo/dependabot/go_modules/github.com/valyala/fasthttp-1.51.0\n\nBump github.com/valyala/fasthttp from 1.50.0 to 1.51.0",
+          "timestamp": "2023-11-21T11:12:10+08:00",
+          "tree_id": "311346c799f03c50e7557984ed49271bd6b2b999",
+          "url": "https://github.com/sunhailin-Leo/triton-service-go/commit/f5b1c32314a29e5808360e4be740e5de98e2b410"
+        },
+        "date": 1700536365830,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkFullTokenizerNotChinese",
+            "value": 20007,
+            "unit": "ns/op\t   11752 B/op\t     288 allocs/op",
+            "extra": "59136 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFullTokenizerChinese",
+            "value": 7137,
+            "unit": "ns/op\t    4272 B/op\t     106 allocs/op",
+            "extra": "165411 times\n4 procs"
           }
         ]
       }
